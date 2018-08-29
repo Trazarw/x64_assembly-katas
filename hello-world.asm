1e6fcfcf1e3005f6246abc97fd48aaa3dd5c753e
MrVoid <enis.nefzi.sanchez@gmail.com>
2018-08-29 22:14:43 +0200

    formating

--- hello-world.asm
+++ hello-world.asm
@@ -1,20 +1,21 @@
 SECTION .data
 
-new_l:  equ 0b1010
-msg:    db "Yo-", "Hello world!", new_l
-msg_l:	equ $-msg
+	new_l:  equ 0b1010
+	msg:    db "Yo-", "Hello world!", new_l
+	msg_l:	equ $-msg
 
 SECTION .text
 
-global _start
+	global _start
 
-_start:
-		mov rax,1
-		mov rdi,1
-		mov rsi,msg
-		mov rdx,msg_l
-		syscall
+		_start:
 
-		mov rax,60
-		mov rdi,0
-		syscall
+			mov rax, 1
+			mov rdi, 1
+			mov rsi, msg
+			mov rdx, msg_l
+			syscall
+
+			mov rax, 60
+			mov rdi, 0
+			syscall
