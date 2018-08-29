0f19ab7f4e3590f07aab5c36d7be70ed9af751e1
MrVoid <enis.nefzi.sanchez@gmail.com>
2018-08-29 23:44:18 +0200

    run and clean

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
-nasm -f elf64 $1.asm 
-ld -m elf_x86_64 -s -o $1_x64 $1.o
-rm $1.o && ./$1_x64
+nasm -f elf64 $1.asm &&
+ld -m elf_x86_64 -s -o $1_x64 $1.o &&
+rm $1.o && ./$1_x64 && rm $1_x64
